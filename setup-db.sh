e110adcecf2e35f774ca0f22383bea930bcb723f
Michael Hargiss <mycargus@users.noreply.github.com>
2016-06-23 19:45:05 -0600

    Update setup-db.sh
    allow custom env vars for db setup

--- setup-db.sh
+++ setup-db.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export CANVAS_LMS_ACCOUNT_NAME=Chiefs
-export CANVAS_LMS_ADMIN_EMAIL=andy.reid@example.com
-export CANVAS_LMS_ADMIN_PASSWORD="password"
-export CANVAS_LMS_STATS_COLLECTION="opt_out"
+if [ -z ${CANVAS_LMS_ACCOUNT_NAME+x} ]; then export CANVAS_LMS_ACCOUNT_NAME=Chiefs; fi
+if [ -z ${CANVAS_LMS_ADMIN_EMAIL+x} ]; then export CANVAS_LMS_ADMIN_EMAIL=andy.reid@example.com; fi
+if [ -z ${CANVAS_LMS_ADMIN_PASSWORD+x} ]; then export CANVAS_LMS_ADMIN_PASSWORD="password"; fi
+if [ -z ${CANVAS_LMS_STATS_COLLECTION+x} ]; then export CANVAS_LMS_STATS_COLLECTION="opt_out"; fi
 
 bundle exec rake db:create \
   && bundle exec rake db:initial_setup
